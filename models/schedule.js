d98f9e6959265a78665e4c3bfde3e8c5f4b95e0c
Taylor Bean <JacobBean18@gmail.com>
2021-03-07 13:33:09 -0500

    Made reccurenceSchema to hold String, Number Pairs

--- models/schedule.js
+++ models/schedule.js
@@ -4,9 +4,14 @@ const GroupMember = require('./groupMember');
 
 function validateReccurenceType(t){
   const legalVals = new Set(['daily', 'weekly', 'monthly']);
-  return legalVals.has(t);
+  return legalVals.has(t.reccurence_name);
 }
 const recurrenceValidator = [validateReccurenceType, '{PATH} does not support value of {VALUE}']
+
+const reccurenceSchema = new Schema({
+  reccurence_name: String,
+  reccurence_days: Number
+})
 
 const ScheduleSchema = new Schema({
   schedule_due_date: Date,
@@ -15,8 +20,7 @@ const ScheduleSchema = new Schema({
     default: 'iterate'
   },
   schedule_recurrence_type: {
-    type: String,
-    default: 'weekly',
+    type: reccurenceSchema,
     validator: recurrenceValidator
   },
   date_created:{

8c61f9b33eda19b8b2719d77f965160125615645
Taylor Bean <JacobBean18@gmail.com>
2021-03-06 19:25:52 -0500

    Added GroupMemberSchema

---
+++ models/groupMember.js
@@ -0,0 +1,22 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const GroupMemberSchema = new Schema({
+  user_ID: Schema.Types.ObjectID,
+  user_name: String,
+  completed_tasks: {
+    type: Number,
+    default: 0,
+    required: true
+  },
+  point_balance: {
+    type: Number,
+    default: 0,
+    required: true
+  }
+
+})
+
+const model = mongoose.model('group', GroupSchema);
+
+module.exports = {model, GroupMemberSchema}

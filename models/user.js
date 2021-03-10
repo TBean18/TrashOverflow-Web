c5bf320e753fffd38f1758ed9a369aa49b349002
Taylor Bean <JacobBean18@gmail.com>
2021-03-10 16:50:06 -0500

    password encryption and addGroup function

--- models/user.js
+++ models/user.js
@@ -1,6 +1,8 @@
 const mongoose = require('mongoose');
 const GroupPlaceHolder = require('./GroupPlaceHolder');
 const Schema = mongoose.Schema;
+const bcrypt = require('bcrypt');
+const SALT_WORK_FACTOR = 10;
 
 const UserSchema = new Schema({
   name:{
@@ -32,6 +34,48 @@ const UserSchema = new Schema({
   }
 
 })
+//A PRE-METHOD that fires before every user.save() call
+//Checks to ensure that the  passwrod was not changed, if it has, then we need to recompute the hash
+UserSchema.pre(save, function(next) {
+  var user = this;
+
+  // only hash the password if it has been modified (or is new)
+  if (!user.isModified('password')) return next();
+
+  // generate a salt
+  bcrypt.genSalt(SALT_WORK_FACTOR, function(err, salt) {
+    if (err) return next(err);
+
+    // hash the password using our new salt
+    bcrypt.hash(user.password_hash, salt, function(err, hash) {
+        if (err) return next(err);
+
+        // override the cleartext password with the hashed one
+        user.password_hash = hash;
+        next();
+    });
+  });
+});
+
+//Compare a plaintext password to the this.password_hash
+UserSchema.methods.comparePassword = function(inputPassword, cb){
+  bcrypt.compare(inputPassword, this.password_hash, (err, isMatch) => {
+    if (err) return cb(err);
+    cb(null, isMatch);
+  });
+};
+
+//Add a group to the users.groups []
+UserSchema.methods.addGroup = function(newGroup, cb){
+  //Format the data from the input model 
+  const data = {
+    group_ID: newGroup._id,
+    group_name: newGroup.group_name
+  }
+  const newGroupHolder = new GroupPlaceHolder.GroupPlaceHolder(data);
+  this.groups.push(newGroupHolder);
+  this.save(cb)
+}
 
 
 const User = mongoose.model('user', UserSchema); 

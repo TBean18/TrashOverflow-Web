cec0e5a599811b5fb04077aef9d309c327140a91
conradsmi <conradsmith22@protonmail.com>
2021-03-12 16:58:08 -0500

    changed frontend/build to frontend/public

--- server.js
+++ server.js
@@ -18,9 +18,9 @@ const url = process.env.MONGO_URI;
 app.use(express.json())
 
 if (process.env.NODE_ENV === 'production') {
-    app.use(express.static('frontend/build'));
+    app.use(express.static('frontend/public'));
     app.get('*', (req, res) => {
-        res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
+        res.sendFile(path.resolve(__dirname, 'frontend', 'public', 'index.html'));
     });
 }
 

5e93c30ecd1d45bb6a87d8743eb20abf2e87f30d
Phil <sheffield712@gmail.com>
2021-03-13 12:29:20 -0500

    Added routes to App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,24 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Redirect, Switch } from 'react-router-dom';
 import './App.css';
 
 import LoginPage from './pages/LoginPage';
+import CardPage from './pages/CardPage';
 
 function App() {
   return (
-    <LoginPage />
+    <Router >
+      <Switch>
+        <Route path="/" exact>
+          <LoginPage />
+        </Route>
+        <Route path="/cards" exact>
+          <CardPage />
+        </Route>
+        <Redirect to="/" />
+      </Switch>  
+    </Router>
   );
 }
 
 export default App;
-

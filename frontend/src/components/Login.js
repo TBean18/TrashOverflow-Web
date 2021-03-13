a2295f4e1b19205f813ddc250a83f353d5475a31
Phil <sheffield712@gmail.com>
2021-03-13 12:29:07 -0500

    Login variables match the API's

--- frontend/src/components/Login.js
+++ frontend/src/components/Login.js
@@ -1,26 +1,34 @@
-import React from 'react';
+import React, { useState } from 'react';
 import "./Login.css";
 
 function Login()
 {
+    let email;
+    let password_hash;
+
+    const [message,setMessage] = useState('');
+
 
     const doLogin = async event => 
     {
         event.preventDefault();
 
-        // alert('doIt()');
+        alert('doIt() ' + email.value + ' ' + password_hash.value );
     };
+
 
     return(
       <div className="LoginArea" id="loginDiv">
         <form onSubmit={doLogin}>
         <span id="inner-title">PLEASE LOG IN</span><br />
-        <input type="text" id="loginName" placeholder="Username" /><br />
-        <input type="password" id="loginPassword" placeholder="Password" /><br />
+        <input type="text" id="email" placeholder="Email" 
+          ref={(c) => email = c} />
+        <input type="password" id="password_hash" placeholder="Password" 
+          ref={(c) => password_hash = c} />
         <input type="submit" id="loginButton" class="buttons" value = "Log In"
           onClick={doLogin} />
         </form>
-        <span id="loginResult"></span>
+        <span id="loginResult">{message}</span>
      </div>
     );
 };

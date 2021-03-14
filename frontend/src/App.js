448249bd297164fdd2d916e7927ce18dc4d8f16e
Taylor Bean <JacobBean18@gmail.com>
2021-03-14 19:28:56 -0400

    Added the GlobalProvider to App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,20 +4,23 @@ import './App.css';
 
 import LoginPage from './pages/LoginPage';
 import CardPage from './pages/CardPage';
+import { GlobalProvider } from './context/GlobalState';
 
 function App() {
   return (
-    <Router >
-      <Switch>
-        <Route path="/" exact>
-          <LoginPage />
-        </Route>
-        <Route path="/cards" exact>
-          <CardPage />
-        </Route>
-        <Redirect to="/" />
-      </Switch>  
-    </Router>
+    <GlobalProvider>
+      <Router >
+        <Switch>
+          <Route path="/" exact>
+            <LoginPage />
+          </Route>
+          <Route path="/cards" exact>
+            <CardPage />
+          </Route>
+          <Redirect to="/" />
+        </Switch>  
+      </Router>
+    </GlobalProvider>
   );
 }
 

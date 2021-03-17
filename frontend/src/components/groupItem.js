cdf92ba4d7db4e46347880d3f1914f856a589a19
Taylor Bean <JacobBean18@gmail.com>
2021-03-17 17:46:29 -0400

    creaeted init groupitem component

---
+++ frontend/src/components/groupItem.js
@@ -0,0 +1,36 @@
+import React, {useState, useContext} from 'react'
+
+
+//Bring in the userState form the global context
+const {user, jwt, storeJWT, storeGroups} = useContext(GlobalContext);
+
+//Function Called for a user to remove him/her self from a group
+function leaveGroup(group_ID){
+    //Make the login API call
+    axios.post('/api/groups/leave', {
+        user_ID: user._id,
+        token: jwt,
+        group_ID
+    })
+    .then(res => {
+        if(res.data.error == '') throw res.data.error
+        console.log(res);
+        storeJWT(res.data.token);
+        storeGroups(res.data.user_groups);
+    })
+    .catch(err => {
+        console.log(err);
+    })
+    return;
+}
+
+export default function groupItem(props) {
+    return (
+        <>
+            <h2>{props.group.group_name}</h2>
+            <p>{props.group.group_description}</p>
+            {/* remove the group on click of this button */}
+            <button onclick={() => leaveGroup(props.group.group_ID)}>x</button>
+        </>
+    )
+}

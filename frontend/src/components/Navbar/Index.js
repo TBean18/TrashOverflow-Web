0f6ad3ec45b063aed6bace306cf68a42082fb50a
Phil <sheffield712@gmail.com>
2021-03-17 16:37:51 -0400

    Navbar just about done

--- frontend/src/components/Navbar/Index.js
+++ frontend/src/components/Navbar/Index.js
@@ -1,11 +1,47 @@
-import React from "react";
+import React from 'react';
+import { FaBars } from 'react-icons/fa';
+import {
+  Nav,
+  NavbarContainer,
+  NavLogo,
+  MobileIcon,
+  NavMenu,
+  NavItem,
+  NavLinks,
+  NavButton,
+  NavButtonLink
+} from './NavbarElements';
 
 const Navbar = () => {
-	return (
-		<div>
-			<h1>Nav</h1>
-		</div>
-	);
+  return (
+    <>
+      <Nav>
+        <NavbarContainer>
+          <NavLogo to="/">TrashOverflow</NavLogo>
+          <MobileIcon>
+            <FaBars />
+          </MobileIcon>
+          <NavMenu>
+            <NavItem>
+              <NavLinks to="about">About</NavLinks>
+            </NavItem>
+            <NavItem>
+              <NavLinks to="discover">Discover</NavLinks>
+            </NavItem>
+            <NavItem>
+              <NavLinks to="services">Services</NavLinks>
+            </NavItem>
+            <NavItem>
+              <NavLinks to="signup">Sign Up</NavLinks>
+            </NavItem>
+          </NavMenu>
+          <NavButton>
+            <NavButtonLink to="/">Sign In</NavButtonLink>
+          </NavButton>
+        </NavbarContainer>
+      </Nav>
+    </>
+  );
 };
 
 export default Navbar;

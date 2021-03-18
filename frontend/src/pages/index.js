074e2d702705b483ae9b1ca35e89ac56158077bc
Phil <sheffield712@gmail.com>
2021-03-18 11:34:15 -0400

    Moves App.js elements to /pages/index.js

---
+++ frontend/src/pages/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Navbar from '../components/Navbar';
+import Sidebar from '../components/Sidebar';
+
+const LandingPage = () => {
+  return (
+    <>
+      <Sidebar />
+      <Navbar />
+    </>
+  );
+};
+
+export default LandingPage;

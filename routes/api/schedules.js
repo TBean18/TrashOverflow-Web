2cbfd35feec05e2d76595e38cca416b516beda8f
Alexa Keene <alexakeene13@gmail.com>
2021-03-17 22:55:49 -0400

    added update schedule endpoint

--- routes/api/schedules.js
+++ routes/api/schedules.js
@@ -6,8 +6,10 @@ const jwt = require('../../util/jwt');
 const schedule = require("../../models/schedule");
 const chore = require("../../models/chore");
 
-// create schedule
-router.post('/createSchedule', (req, res) => {
+// Route        POST api/schedules/
+// Description  Creates a new schedule and links it to a chore. 
+// Access       Public
+router.post('/create', (req, res) => {
     // TODO: possibly make this admin only priviledges.
 
     // Search the database for the chore.
@@ -20,30 +22,66 @@ router.post('/createSchedule', (req, res) => {
                 payload["schedule_due_date"] = req.body["schedule_due_date"];
             if (req.body.hasOwnProperty("schedule_user_rotation_type"))
                 payload["schedule_user_rotation_type"] = req.body["schedule_user_rotation_type"];
-            
+
             // This may need to change depending on how the data gets sent from backend. 
             if (req.body.hasOwnProperty("schedule_recurrence_type"))
                 payload["schedule_recurrence_type"] = req.body["schedule_recurrence_type"];
 
             const newSched = new schedule(payload);
-            // FIXME: do we save this in schedule or chore?
+            // Save the schedule into the database.
             newSched.save()
                 .then(item => res.json(item));
+            .catch(err => {
+                console.log(err);
+                res.json({
+                    error: err
+                });
+            });
+
+            // Link the schedule to the chore.
+            c.chore_schedule = newSched;
+        })
+        .catch(err => {
+            console.log(err);
+            res.json({
+                error: err
+            });
+        })
+});
+
+// Route        POST api/schedules/
+// Description  Updates the schedule. 
+// Access       Public
+// Note: This endpoints updates everything passed even if it is not actually different.
+router.post('/edit', (req, res) => {
+    // TODO: possibly make this admin only priviledges. 
+
+    chore.findById(req.body._id)
+        .then(c => {
+            const payload = {};
+            if (req.body.hasOwnProperty("schedule_due_date"))
+                payload["schedule_due_date"] = req.body["schedule_due_date"];
+            if (req.body.hasOwnProperty("schedule_user_rotation_type"))
+                payload["schedule_user_rotation_type"] = req.body["schedule_user_rotation_type"];
+
+            // This may need to change depending on how the data gets sent from backend. 
+            if (req.body.hasOwnProperty("schedule_recurrence_type"))
+                payload["schedule_recurrence_type"] = req.body["schedule_recurrence_type"];
+
+            // Updates the chore schedule object associated with the chore found. 
+            c.chore_schedule.update(playload)
+                .then(item => res.json(item))
                 .catch(err => {
                     console.log(err);
-                    res.json({error: err});
+                    res.json({
+                        error: err
+                    });
                 });
         })
         .catch(err => {
             console.log(err);
-            res.json({error: err});
-        })
-
-});
-
-// update schedule
-router.post('/updateSchedule', (req, res) => {
-    // TODO: possibly make this admin only priviledges. 
-
-
+            res.json({
+                error: err
+            });
+        });
 });

7d57a32d1240a551512c1253238e4dc34f4c3488
Jo Johnson <jojohnson.jsj@gmail.com>
2021-03-18 16:28:01 -0400

    Initial. Nav bar and Sidebar bases complete

---
+++ trash-overflow/src/App.js
@@ -0,0 +1,21 @@
+import React from "react";
+import './App.css';
+import Header from './Header';
+import Sidebar from "./Sidebar";
+
+function App() {
+  return (
+    // BEM naming convention
+    <div className="app">
+      <Header />
+      
+      <div className="app__body">
+        <Sidebar />
+        {/* Feed */}
+        {/* Widgets */}
+      </div>
+    </div>
+  );
+}
+
+export default App;

b715b9bafc4179238e48ac6887d09a767ec98a0e
Phil <sheffield712@gmail.com>
2021-03-19 17:35:50 -0400

    Add video to landing page

--- frontend/src/pages/index.js
+++ frontend/src/pages/index.js
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import Hero from '../components/Hero';
 import Navbar from '../components/Navbar';
 import Sidebar from '../components/Sidebar';
 
@@ -15,6 +16,7 @@ const LandingPage = () => {
     <>
       <Sidebar isOpen={isOpen} toggle={toggle} />
       <Navbar toggle={toggle} />
+      <Hero />
     </>
   );
 };

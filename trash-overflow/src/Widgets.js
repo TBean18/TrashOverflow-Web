e93cd03ef7a9e4dfe1401625fd670627a7b82bf7
Jo Johnson <jojohnson.jsj@gmail.com>
2021-03-19 16:04:15 -0400

    Added dark theme

--- trash-overflow/src/Widgets.js
+++ trash-overflow/src/Widgets.js
@@ -1,9 +1,10 @@
 import React from 'react'
-import './Widgets.css'
 
 function Widgets() {
     return (
-        <div className="widgets">
+        <div className="widgets" width="340"
+        height="100%">
+        {/*
             <iframe
                 src="https://www.facebook.com/plugins/page.php?
                     href=https%3A%2F%2Fwww.facebook.com%2FCleverProgrammerr%2F&
@@ -20,6 +21,7 @@ function Widgets() {
                     allow="encrypted-media"
                 >
             </iframe>
+        */}
         </div>
     )
 }

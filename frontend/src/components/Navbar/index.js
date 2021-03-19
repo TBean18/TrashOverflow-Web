2f5026f4e12f2f9cee8a4d79b3043f76f6dd10e8
Phil <sheffield712@gmail.com>
2021-03-19 16:28:22 -0400

    Add toggle features to hamburger icon

--- frontend/src/components/Navbar/index.js
+++ frontend/src/components/Navbar/index.js
@@ -12,13 +12,13 @@ import {
   NavButtonLink
 } from './NavbarElements';
 
-const Navbar = () => {
+const Navbar = ({ toggle }) => {
   return (
     <>
       <Nav>
         <NavbarContainer>
           <NavLogo to="/">TrashOverflow</NavLogo>
-          <MobileIcon>
+          <MobileIcon onClick={toggle}>
             <FaBars />
           </MobileIcon>
           <NavMenu>

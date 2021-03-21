53f62e12a71016136c8c0645be635a65d8a57041
Phil <sheffield712@gmail.com>
2021-03-21 19:35:25 -0400

    Fix formatting issues on landing page

--- frontend/src/components/Hero/HeroElements.js
+++ frontend/src/components/Hero/HeroElements.js
@@ -6,7 +6,7 @@ export const HeroContainer = styled.div`
   justify-content: center;
   align-items: center;
   padding: 0 30px;
-  height: 800px;
+  height: 80vh;
   position: relative;
   z-index: 1;
 `;

aa0608d127d9532a25439754148a4258bcb87897
Phil <sheffield712@gmail.com>
2021-03-21 19:36:35 -0400

    Move about/discover/services further right

--- frontend/src/components/Navbar/NavbarElements.js
+++ frontend/src/components/Navbar/NavbarElements.js
@@ -62,7 +62,7 @@ export const NavMenu = styled.ul`
   align-items: center;
   list-style: none;
   text-align: center;
-  margin-right: -22px;
+  margin-right: -220px;
 
   @media screen and (max-width: 768px) {
     display: none;

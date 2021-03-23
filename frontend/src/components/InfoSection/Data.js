bb488f1623107b63e38c48456e7b0bc49e87050d
Phil <sheffield712@gmail.com>
2021-03-23 19:11:28 -0400

    Create info sections for landing page

---
+++ frontend/src/components/InfoSection/Data.js
@@ -0,0 +1,55 @@
+export const homeObjOne = {
+  id: 'about',
+  lightBg: false,
+  lightText: true,
+  lightTextDesc: true,
+  topLine: 'Top Line 1',
+  headline: 'Headline 1 Goes Here',
+  description: 'Make sure to put a description here.',
+  buttonLabel: 'Get Started',
+  imgStart: false,
+  img: require('../../images/svg-3.svg').default,
+  alt: 'Laptop',
+  dark: true,
+  primary: true,
+  darkText: false
+};
+
+export const homeObjTwo = {
+  id: 'discover',
+  // Controls the background of the section. True is white
+  lightBg: true,
+  // Controls color of headline. False is black.
+  lightText: false,
+  lightTextDesc: false,
+  topLine: 'Top Line 2',
+  headline: 'Headline 2 Goes Here',
+  description: 'Make sure to put a description here.',
+  buttonLabel: 'Get Started',
+  imgStart: true,
+  img: require('../../images/svg-2.svg').default,
+  alt: 'Laptop',
+  // Controls font color within button. False is white
+  dark: false,
+  // Primary color... i.e. orange
+  primary: false,
+  // Controls description color. True is black.
+  darkText: true
+};
+
+export const homeObjThree = {
+  id: 'about',
+  lightBg: false,
+  lightText: true,
+  lightTextDesc: true,
+  topLine: 'Top Line 3',
+  headline: 'Headline 3 Goes Here',
+  description: 'Make sure to put a description here.',
+  buttonLabel: 'Get Started',
+  imgStart: false,
+  img: require('../../images/svg-1.svg').default,
+  alt: 'Laptop',
+  dark: true,
+  primary: true,
+  darkText: false
+};

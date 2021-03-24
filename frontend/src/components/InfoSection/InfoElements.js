c9dcde080bdbd157957d47e5b7d6f3d7e072bfe2
Phil <sheffield712@gmail.com>
2021-03-24 10:37:40 -0400

    Fix formatting on mobile

--- frontend/src/components/InfoSection/InfoElements.js
+++ frontend/src/components/InfoSection/InfoElements.js
@@ -30,7 +30,7 @@ export const InfoRow = styled.div`
 
   @media screen and (max-width: 768px) {
     grid-template-areas: ${({ imgStart }) =>
-      imgStart ? `'col1' 'col2'` : `'col1 col1' 'col2 col 2'`};
+      imgStart ? `'col1' 'col2'` : `'col1 col1' 'col2 col2'`};
   }
 `;
 

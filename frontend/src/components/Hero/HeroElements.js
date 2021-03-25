d8a4a880db03d2a4b5f3dee494e38efc67f93913
Phil <sheffield712@gmail.com>
2021-03-25 13:43:19 -0400

    Fix extra parentheses

--- frontend/src/components/Hero/HeroElements.js
+++ frontend/src/components/Hero/HeroElements.js
@@ -19,13 +19,12 @@ export const HeroContainer = styled.div`
     right: 0;
     bottom: 0;
     background: linear-gradient(
-      180deg,
-      rgba(0, 0, 0, 0.2) 0%,
-      rgba(0, 0, 0, 0.6) 100%
-    ),
+        180deg,
+        rgba(0, 0, 0, 0.2) 0%,
+        rgba(0, 0, 0, 0.6) 100%
+      ),
       linear-gradient(180deg, rgba(0, 0, 0, 0.2) 0%, transparent 100%);
-      z-index: 2;
-    )
+    z-index: 2;
   }
 `;
 

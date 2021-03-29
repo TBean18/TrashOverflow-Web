6165513401602e0859bb400d3b52325fcdf57323
Taylor Bean <jacobbean18@gmail.com>
2021-03-29 01:55:26 -0400

    now works with test client Ethereal

--- util/mailer.js
+++ util/mailer.js
@@ -1,29 +1,27 @@
 const nodemailer = require("nodemailer");
-let testAccount = nodemailer.createTestAccount();
 
-//Transporter is the object that sends emails
-let transporter = nodemailer.createTransporter({
-  host: "smtp.ethereal.email",
-  port: 587,
-  secure: false, // true for 465, false for other ports
-  auth: {
-    user: testAccount.user, // generated ethereal user
-    pass: testAccount.pass, // generated ethereal password
-  },
-});
+exports.sendVerficationEmail = async function (emailAddress, token, cb) {
+  //Create A Test Account on Ethereal
+  let testAccount = await nodemailer.createTestAccount();
 
-exports.sendVerficationEmail = function (emailAddress, token) {
+  //Transporter is the object that sends emails
+  let transporter = nodemailer.createTransport({
+    host: "smtp.ethereal.email",
+    port: 587,
+    secure: false, // true for 465, false for other ports
+    auth: {
+      user: testAccount.user, // generated ethereal user
+      pass: testAccount.pass, // generated ethereal password
+    },
+  });
   transporter.sendMail(
     {
       from: '"TrashOverflow" <noreply@trashoverflow.tech>',
       to: emailAddress,
       subject: "TrashOverflow | Email Verification",
       text: "Please click on the link below to verfiy your email",
-      html: "<a href='www.twitter.com'>LINK</a>",
+      html: '<a href="https://www.twitter.com">LINK</a>',
     },
-    (err, info) => {
-      if (err) throw err;
-      console.log(info);
-    }
+    cb
   );
 };

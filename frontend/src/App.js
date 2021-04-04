33e09e59401198803976f85ce41ac1e184eb58cd
Taylor Bean <JacobBean18@gmail.com>
2021-04-03 22:37:21 -0400

    Added a query global Provider

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,49 +1,55 @@
-import React from 'react';
+import React from "react";
 import {
   BrowserRouter as Router,
   Route,
   Redirect,
-  Switch
-} from 'react-router-dom';
-import './App.css';
+  Switch,
+} from "react-router-dom";
+import "./App.css";
 
 // import CardPage from './pages/CardPage';
-import RegisterPage from './pages/RegisterPage';
-import LandingPage from './pages/LandingPage';
-import ForgotPassPage from './pages/ForgotPassPage';
-import Main from './js/Main';
+import RegisterPage from "./pages/RegisterPage";
+import LandingPage from "./pages/LandingPage";
+import ForgotPassPage from "./pages/ForgotPassPage";
+import Main from "./js/Main";
 
-import { GlobalProvider } from './context/GlobalState';
-import GroupView from './js/GroupView';
-import Login from './components/Login';
+import { GlobalProvider } from "./context/GlobalState";
+import GroupView from "./js/GroupView";
+import Login from "./components/Login";
+import { QueryClientProvider } from "react-query";
+import useQueryClientCreator from "./hooks/useQueryClientCreator";
 
 function App() {
+  const queryClient = useQueryClientCreator();
+
   return (
-    <GlobalProvider>
-      <Router>
-        <Switch>
-          <Route path="/" exact>
-            <LandingPage />
-          </Route>
-          <Route path="/chores" exact>
-            <Main />
-          </Route>
-          <Route path="/groups" exact>
-            <GroupView />
-          </Route>
-          <Route path="/signin" exact>
-            <Login />
-          </Route>
-          <Route path="/register" exact>
-            <RegisterPage />
-          </Route>
-          <Route path="/forgot" exact>
-            <ForgotPassPage />
-          </Route>
-          <Redirect to="/" />
-        </Switch>
-      </Router>
-    </GlobalProvider>
+    <QueryClientProvider client={queryClient}>
+      <GlobalProvider>
+        <Router>
+          <Switch>
+            <Route path="/" exact>
+              <LandingPage />
+            </Route>
+            <Route path="/chores" exact>
+              <Main />
+            </Route>
+            <Route path="/groups" exact>
+              <GroupView />
+            </Route>
+            <Route path="/signin" exact>
+              <Login />
+            </Route>
+            <Route path="/register" exact>
+              <RegisterPage />
+            </Route>
+            <Route path="/forgot" exact>
+              <ForgotPassPage />
+            </Route>
+            <Redirect to="/" />
+          </Switch>
+        </Router>
+      </GlobalProvider>
+    </QueryClientProvider>
   );
 }
 

e28ce9f7ab4a679c7e91d940e5a862a66d744543
Phil <sheffield712@gmail.com>
2021-04-05 09:35:32 -0400

    Add Sidebar folder to /src/js

--- frontend/src/js/Main.js
+++ frontend/src/js/Main.js
@@ -1,19 +1,19 @@
-import React from "react";
+import React from 'react';
 import '../css/Main.css';
 
-import Feed from "./Feed";
+import Feed from './Feed';
 import Header from './Header';
-import Sidebar from "./Sidebar";
-import Widgets from "./Widgets";
+import Sidebar from './Sidebar/Sidebar';
+import Widgets from './Widgets';
 
 function Main() {
   return (
     // BEM naming convention
 
     <div className="main">
-      <Header selection={1}/>
+      <Header selection={1} />
       <Sidebar />
-      
+
       <div className="main__body">
         <Feed />
         <Widgets />

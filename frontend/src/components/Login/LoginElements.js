37bfafc69de52137d284ef0f4deb2c5b1a462bc3
Phil <sheffield712@gmail.com>
2021-04-05 10:35:15 -0400

    Clean up mobile login page for smaller phones

--- frontend/src/components/Login/LoginElements.js
+++ frontend/src/components/Login/LoginElements.js
@@ -1,5 +1,5 @@
-import styled from "styled-components";
-import { Link } from "react-router-dom";
+import styled from 'styled-components';
+import { Link } from 'react-router-dom';
 
 export const Container = styled.div`
   min-height: 692px;
@@ -79,6 +79,7 @@ export const Form = styled.form`
   }
   @media screen and (max-width: 350px) {
     padding: 16px 32px;
+    margin-top: -40px;
     margin-bottom: 50px;
   }
 `;
@@ -135,7 +136,7 @@ export const FormButton = styled.button`
 export const Text = styled.text`
   max-width: 330px;
   text-align: center;
-  margin-top: 24px;
+  margin-top: 14px;
   font-size: 14px;
   text-overflow: ellipsis;
   white-space: nowrap;
@@ -145,7 +146,7 @@ export const Text = styled.text`
 
 export const TextL = styled(Link)`
   text-align: center;
-  margin-top: 24px;
+  margin-top: 14px;
   color: #000;
   font-size: 14px;
 `;

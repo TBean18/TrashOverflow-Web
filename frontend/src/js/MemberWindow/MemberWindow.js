7f11fd4507e5baa9f5eeff9ae1b29cad4bd43f36
Taylor Bean <JacobBean18@gmail.com>
2021-04-09 19:06:49 -0400

    now displays members

--- frontend/src/js/MemberWindow/MemberWindow.js
+++ frontend/src/js/MemberWindow/MemberWindow.js
@@ -1,43 +1,40 @@
-import React from 'react';
-import '../../css/MemberWindow.css';
-import MemberWindowMember from './MemberWindowMember';
-import JoIcon from '../../public/images/JoIcon.png';
-import onClickOutside from 'react-onclickoutside';
+import React from "react";
+import "../../css/MemberWindow.css";
+import MemberWindowMember from "./MemberWindowMember";
+import JoIcon from "../../public/images/JoIcon.png";
+import onClickOutside from "react-onclickoutside";
 
 class MemberWindow extends React.Component {
   handleClickOutside = () => {
     this.props.hideMembers();
-  }
-  
+  };
+
   render() {
-    
+    const { members } = this.props;
+
+    function displayMembers(members) {
+      if (!Array.isArray(members)) return;
+      return members.map((member) => (
+        <MemberWindowMember name={member.user_name} />
+      ));
+    }
+
     return (
       <div>
-          <div className="memberWindow">
-            <div className="memberWindow__top">
-              <h4>Members</h4>
+        <div className="memberWindow">
+          <div className="memberWindow__top">
+            <h4>Members</h4>
+          </div>
+          <div className="memberWindow__body">
+            <div className="memberWindow__bodyInput">
+              <input placeholder="Search Members" type="text" />
             </div>
-            <div className="memberWindow__body">
-              <div className="memberWindow__bodyInput">
-                <input placeholder="Search Members" type="text" />
-              </div>
-              <div className="memberWindow__member">
-                <MemberWindowMember
-                  src="https://material-ui.com/static/images/avatar/1.jpg"
-                  name="Danesh Balmer"
-                />
-                <MemberWindowMember
-                  src="https://material-ui.com/static/images/avatar/3.jpg"
-                  name="Tracy Kepler"
-                />
-                <MemberWindowMember
-                  src="https://material-ui.com/static/images/avatar/2.jpg"
-                  name="Ron Torkelson"
-                />
-                <MemberWindowMember src={JoIcon} name="Jo Johnson" />
-              </div>
+            <div className="memberWindow__member">
+              {displayMembers(members)}
+              <MemberWindowMember src={JoIcon} name="Jo Johnson" />
             </div>
           </div>
+        </div>
       </div>
     );
   }

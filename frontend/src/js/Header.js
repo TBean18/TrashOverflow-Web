51decfe742e593db0f9898ff212875df342a8dea
Taylor Bean <JacobBean18@gmail.com>
2021-04-09 19:19:54 -0400

    Header now displays correct Icon

--- frontend/src/js/Header.js
+++ frontend/src/js/Header.js
@@ -1,5 +1,5 @@
 import React, { useContext } from "react";
-import logo from "../public/images/logo.png";
+import logo from "../public/images/Trash_Overflow_Icon_Test.svg";
 import "../css/Header.css";
 import { NavLink } from "react-router-dom";
 

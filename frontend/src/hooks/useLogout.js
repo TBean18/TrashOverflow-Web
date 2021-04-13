80df492c672488fcc7a3122c6b78c97a18915ed1
Taylor Bean <JacobBean18@gmail.com>
2021-04-13 13:31:31 -0400

    removed params

--- frontend/src/hooks/useLogout.js
+++ frontend/src/hooks/useLogout.js
@@ -2,7 +2,7 @@ import react, { useContext } from "react";
 import axios from "axios";
 import { useQuery } from "react-query";
 
-export default function useLogout(globalContext) {
+export default function useLogout() {
   localStorage.clear();
   window.location.href = "/signin";
 }

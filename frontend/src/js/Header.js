1baf11767a6d485658467cb187e2afe381a65d78
Taylor Bean <JacobBean18@gmail.com>
2021-04-13 19:17:29 -0400

    removed useless imports; displays logged in user's name

--- frontend/src/js/Header.js
+++ frontend/src/js/Header.js
@@ -1,37 +1,22 @@
-import React from 'react';
-import logo from '../public/images/logo.png';
-import '../css/Header.css';
-import { NavLink } from 'react-router-dom';
-import SearchIcon from '@material-ui/icons/Search';
-import HomeIcon from '@material-ui/icons/Home';
-import FlagIcon from '@material-ui/icons/Flag';
-import GroupOutlinedIcon from '@material-ui/icons/GroupOutlined';
-import StorefrontOutlinedIcon from '@material-ui/icons/StorefrontOutlined';
-import SupervisedUserCircleOutlinedIcon from '@material-ui/icons/SupervisedUserCircleOutlined';
-import { Avatar, IconButton } from '@material-ui/core';
-import ExitToAppIcon from '@material-ui/icons/ExitToApp';
-import PostOption from './Post/PostOption';
-import SearchIcon from "@material-ui/icons/Search";
+import React, { useContext } from "react";
+import logo from "../public/images/Trash_Overflow_Icon_Test.svg";
+import "../css/Header.css";
+import { NavLink } from "react-router-dom";
 import HomeIcon from "@material-ui/icons/Home";
-import FlagIcon from "@material-ui/icons/Flag";
-import GroupOutlinedIcon from "@material-ui/icons/GroupOutlined";
-import StorefrontOutlinedIcon from "@material-ui/icons/StorefrontOutlined";
 import SupervisedUserCircleOutlinedIcon from "@material-ui/icons/SupervisedUserCircleOutlined";
-import { Avatar, IconButton } from "@material-ui/core";
-import AddIcon from "@material-ui/icons/Add";
-import ForumIcon from "@material-ui/icons/Forum";
-import NotificationsActiveIcon from "@material-ui/icons/NotificationsActive";
-import ExpandMoreIcon from "@material-ui/icons/ExpandMore";
+import { Avatar } from "@material-ui/core";
+import ExitToAppIcon from "@material-ui/icons/ExitToApp";
+import PostOption from "./Post/PostOption";
 import { GlobalContext } from "../context/GlobalState";
-function Header({ selection }) {
+function Header() {
   const { user } = useContext(GlobalContext);
 
   return (
     <div className="header">
       <div className="header__left">
-      <div className="header__info">
-          <Avatar />
-          <h4>jojohnson.jsj</h4>
+        <div className="header__info">
+          <Avatar src={logo} />
+          <h4>{user.name}</h4>
         </div>
       </div>
 
@@ -53,13 +38,12 @@ function Header({ selection }) {
         </NavLink>
       </div>
       <div className="header__right">
-        <PostOption 
+        <PostOption
           className="header__logout"
           Icon={ExitToAppIcon}
           title="Logout"
           color="grey"
         />
-
       </div>
     </div>
   );

ca568a5df111746cc5d3c9ee779a4b5cb248bac8
Taylor Bean <JacobBean18@gmail.com>
2021-04-13 14:28:14 -0400

    new logout function

--- frontend/src/hooks/useLogout.js
+++ frontend/src/hooks/useLogout.js
@@ -2,7 +2,11 @@ import react, { useContext } from "react";
 import axios from "axios";
 import { useQuery } from "react-query";
 
+//Function used to lof the user out of the application
 export default function useLogout() {
-  localStorage.clear();
-  window.location.href = "/signin";
+  return () => {
+    localStorage.clear();
+    console.log("LOGOUT");
+    window.location.href = "/signin";
+  };
 }

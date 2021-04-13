93c4eecdde4e51c310b5a6540a772702c6484161
Taylor Bean <JacobBean18@gmail.com>
2021-04-13 13:25:25 -0400

    formatted, logout

--- frontend/src/context/GlobalState.js
+++ frontend/src/context/GlobalState.js
@@ -1,89 +1,83 @@
-import React, { createContext, useReducer, useState} from 'react';
-import AppReducer from './AppReducer'
-const axios = require('axios').default;
+import React, { createContext, useReducer, useState } from "react";
+import AppReducer from "./AppReducer";
+const axios = require("axios").default;
 
 //Initial State
 const initialState = {
-    user: JSON.parse(localStorage.getItem('user')) || '',
-    groups: JSON.parse(localStorage.getItem('groups')) || '',
-    jwt: JSON.parse(localStorage.getItem('JWT')) || ''
+  user: JSON.parse(localStorage.getItem("user")) || "",
+  groups: JSON.parse(localStorage.getItem("groups")) || "",
+  jwt: JSON.parse(localStorage.getItem("JWT")) || "",
 };
 
 // Use the Initial State decs to set the default 'x-auth-token' header
-axios.defaults.headers.common['x-auth-token'] = initialState.jwt;
-
+axios.defaults.headers.common["x-auth-token"] = initialState.jwt;
 
 //Create a new Context
 export const GlobalContext = createContext(initialState);
 
 //Create a global Provider
 export const GlobalProvider = function (props) {
+  // const [state, dispatch] = useReducer(AppReducer, initialState);
 
+  //Create the user State
+  const [user, setUser] = useState(initialState.user);
+  //Create the JWT state
+  const [jwt, setJWT] = useState(initialState.jwt);
+  //Create the Groups State
+  const [groups, setGroups] = useState(initialState.groups);
 
-    // const [state, dispatch] = useReducer(AppReducer, initialState);
+  //ACTIONS
 
-    //Create the user State
-    const [user, setUser] = useState(initialState.user);
-    //Create the JWT state
-    const [jwt, setJWT] = useState(initialState.jwt);
-    //Create the Groups State
-    const [groups, setGroups] = useState(initialState.groups)
+  //Function used to store the JWT token from the API responce
+  function storeJWT(webToken) {
+    if (webToken == undefined) return;
+    localStorage.setItem("JWT", JSON.stringify(webToken));
+    axios.defaults.headers.common["x-auth-token"] = webToken;
+    setJWT(webToken);
+  }
 
-    //ACTIONS
+  //Login function used to store userData in global state
+  function logIn(userObject, webToken) {
+    if (userObject == undefined) return;
+    setUser(userObject);
+    localStorage.setItem("user", JSON.stringify(userObject));
+    storeJWT(webToken);
 
-    //Function used to store the JWT token from the API responce
-    function storeJWT(webToken){
-        if(webToken == undefined) return;
-        localStorage.setItem('JWT', JSON.stringify(webToken));
-        axios.defaults.headers.common['x-auth-token'] = webToken;
-        setJWT(webToken);
-    } 
+    //Gather the group info from
+    let curGroups = [];
+    userObject.groups.map((g) => {
+      curGroups.push(g.group_ID);
+    });
+    storeGroups(curGroups);
+  }
 
-    //Login function used to store userData in global state
-    function logIn(userObject, webToken){
-        if(userObject == undefined) return;
-        setUser(userObject);
-        localStorage.setItem('user', JSON.stringify(userObject));
-        storeJWT(webToken)
-        
-        //Gather the group info from 
-        let curGroups = []
-        userObject.groups.map(g =>{
-            curGroups.push(g.group_ID);
-        });
-        storeGroups(curGroups);
-    }
+  //Logout function used to remove user data from global state
+  function logOut(id) {
+    localStorage.setItem("user", "");
+    setUser("");
+    storeJWT("");
+    storeGroups("");
+  }
 
-    //Logout function used to remove user data from global state
-    function logOut(id){
-        localStorage.setItem('user', '');
-        setUser('');
-        // dispatch({
-        //     type: 'LOGOUT',
-        //     payload: id
-        // });
-    }
+  function storeGroups(curGroups) {
+    localStorage.setItem("groups", JSON.stringify(curGroups));
+    setGroups(curGroups);
+  }
 
-    function storeGroups(curGroups){
-        localStorage.setItem('groups', JSON.stringify(curGroups));
-        setGroups(curGroups);
-    }
-
-
-
-    //What the GlobalProvider componet 'renders'
-    return(
-        <GlobalContext.Provider value = {{
-            user: user,
-            jwt: jwt,
-            groups,
-            logIn,
-            logOut,
-            storeJWT,
-            storeGroups
-        }}>
-            {props.children}
-        </GlobalContext.Provider>
-    );
-
-}
+  //What the GlobalProvider componet 'renders'
+  return (
+    <GlobalContext.Provider
+      value={{
+        user: user,
+        jwt: jwt,
+        groups,
+        logIn,
+        logOut,
+        storeJWT,
+        storeGroups,
+      }}
+    >
+      {props.children}
+    </GlobalContext.Provider>
+  );
+};

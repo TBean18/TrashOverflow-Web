1eb5663f1924734d49acffccc6b412e5de02da1d
Taylor Bean <JacobBean18@gmail.com>
2021-04-13 15:12:43 -0400

    now uses useAPIErrorChecking

--- frontend/src/hooks/useUserChores.js
+++ frontend/src/hooks/useUserChores.js
@@ -3,11 +3,17 @@
 import react from "react";
 import axios from "axios";
 import { useQuery } from "react-query";
+import { useAPIErrorChecking } from "./useAPIErrorChecking";
 
 export default function useUserChores() {
+  const errCheck = useAPIErrorChecking();
   return useQuery(
     ["user", "chores"],
-    () => axios.get("/api/chores/user_chores").then((res) => res.data),
+    () =>
+      axios
+        .get("/api/chores/user_chores")
+        .then((res) => res.data)
+        .catch((err) => errCheck(err)),
     {
       // onSuccess: store the token
     }

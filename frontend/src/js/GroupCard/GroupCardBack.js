7e4a2c981c0a11e44989956d46f78ae2f7219268
Taylor Bean <JacobBean18@gmail.com>
2021-04-13 16:19:39 -0400

    Fixed the merge errors

--- frontend/src/js/GroupCard/GroupCardBack.js
+++ frontend/src/js/GroupCard/GroupCardBack.js
@@ -1,17 +1,16 @@
-import React, { useContext } from 'react';
-import '../../css/GroupCardBack.css';
-import CheckCircleOutlineIcon from '@material-ui/icons/CheckCircleOutline';
-import EditOutlinedIcon from '@material-ui/icons/EditOutlined';
-import { GlobalContext } from '../../context/GlobalState';
-import 'react-awesome-button/dist/themes/theme-blue.css';
-import GroupCardMember from '../../components/GroupView/GroupCardMember';
-import PostOption from '../Post/PostOption';
-import { NavLink } from 'react-router-dom';
-
+import React, { useContext } from "react";
+import "../../css/GroupCardBack.css";
+import CheckCircleOutlineIcon from "@material-ui/icons/CheckCircleOutline";
+import EditOutlinedIcon from "@material-ui/icons/EditOutlined";
+import { GlobalContext } from "../../context/GlobalState";
+import "react-awesome-button/dist/themes/theme-blue.css";
+import GroupCardMember from "../../components/GroupView/GroupCardMember";
+import PostOption from "../Post/PostOption";
+import { NavLink } from "react-router-dom";
 
 function GroupCardBack({ curGroup }) {
   const { groups } = useContext(GlobalContext);
-
+  const groupChoresURL = `/groupchores/${curGroup._id}`;
   function displayMembers(group) {
     let members = group.group_members;
     if (!Array.isArray(members)) return;
@@ -24,42 +23,29 @@ function GroupCardBack({ curGroup }) {
     <div className="groupCardBack">
       <div className="groupCardBack__body">
         <h4>Description</h4>
-        <p>There is no tracking information for the current branch.
-           Please specify which branch you want to merge with.
-           See git-pull(1) for details.
-        </p>
-
+        <p>{curGroup.group_description}</p>
       </div>
       <div className="groupCardBack__bottom">
         <h4>Members</h4>
         <div className="groupCardBack__bottomMembers">
-          <>{displayMembers(groups)}</>
-          <GroupCardMember name="Jason Terry" points={31} />
-          <GroupCardMember name="Jason Terry" points={31} />
-          <GroupCardMember name="Jason Terry" points={31} />
-          <GroupCardMember name="Jason Terry" points={31} />
-          <GroupCardMember name="Jason Terry" points={31} />
+          <>{displayMembers(curGroup)}</>
           <GroupCardMember name="Jason Terry" points={31} />
           <GroupCardMember name="Jason Terry" points={31} />
         </div>
       </div>
       <div className="groupCardBack__button">
         <NavLink
-            to="/groupchores"
-            className="groupCardBack__buttonLink"
-            style={{ textDecoration: 'none' }}
-          >
+          to={groupChoresURL}
+          className="groupCardBack__buttonLink"
+          style={{ textDecoration: "none" }}
+        >
           <PostOption
-              Icon={CheckCircleOutlineIcon}
-              title="Select"
-              color="grey"
+            Icon={CheckCircleOutlineIcon}
+            title="Select"
+            color="grey"
           />
         </NavLink>
-        <PostOption
-              Icon={EditOutlinedIcon}
-              title="Edit"
-              color="grey"
-          />
+        <PostOption Icon={EditOutlinedIcon} title="Edit" color="grey" />
       </div>
     </div>
   );

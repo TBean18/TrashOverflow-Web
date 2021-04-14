d3fd93863c57fd5a4324a0810b5c8a66f9b23b75
Taylor Bean <JacobBean18@gmail.com>
2021-04-14 14:49:50 -0400

    GroupMember Avatar background are generated from names

--- frontend/src/js/MemberWindow/MemberWindowMember.js
+++ frontend/src/js/MemberWindow/MemberWindowMember.js
@@ -1,12 +1,40 @@
-import React from 'react';
-import '../../css/MemberWindowMember.css';
-import { Avatar } from '@material-ui/core';
-import DoneOutlinedIcon from '@material-ui/icons/DoneOutlined';
+import React from "react";
+import "../../css/MemberWindowMember.css";
+import { Avatar } from "@material-ui/core";
+import DoneOutlinedIcon from "@material-ui/icons/DoneOutlined";
+import { makeStyles } from "@material-ui/core/styles";
+import { ThemeConsumer } from "styled-components";
+
+const useStyles = makeStyles((theme) => ({
+  CustomColors: (props) => ({
+    backgroundColor: `hsl(${props.backgroundColor}, 50%, 50%)`,
+    // color: theme.palette.getContrastText(props.backgroundColor),
+  }),
+}));
+
+const colorGenerator = (name) => {
+  let hash = 0;
+  for (let i = 0; i < name.length; i++) {
+    hash = name.charCodeAt(i) + ((hash << 5) - hash);
+  }
+  return hash % 360;
+};
 
 function MemberWindowMember({ src, name }) {
+  const color = colorGenerator(name);
+  const props = {
+    backgroundColor: color,
+  };
+  const classes = useStyles(props);
+
+  let abrev = "";
+  name.split(" ").map((word) => {
+    abrev += word.charAt(0).toUpperCase();
+  });
   return (
     <div className="memberWindowMember">
-      {src && <Avatar src={src} />}
+      <Avatar className={classes.CustomColors}>{abrev}</Avatar>
+
       <h4>{name}</h4>
       <DoneOutlinedIcon />
     </div>

0e5b9783a2055aada918db9125e21cc687f5722b
Taylor Bean <JacobBean18@gmail.com>
2021-04-19 21:11:42 -0400

    added an _id prop to populateChoreList method

--- models/group.js
+++ models/group.js
@@ -161,6 +161,7 @@ GroupSchema.methods.populateChoreList = function (choreList, cb) {
       ),
       chore_point_value: choreList[i].chore_point_value,
       chore_completion_status: choreList[i].chore_completion_status,
+      _id: choreList[i]._id,
     };
 
     ret.push(obj);
@@ -209,10 +210,11 @@ GroupSchema.statics.editChore = function (IDs, updates) {
 GroupSchema.methods.rotateAssignedUser = function (chore_index, save, cb) {
   //Set the assigned_user to the next user in the user_pool
   this.group_chores[chore_index].chore_assigned_user_index =
-    (this.group_chores[chore_index].chore_assigned_user_index + 1) % this.group_chores[chore_index].chore_user_pool.length;
-  this.group_chores[chore_index].chore_assigned_user = this.group_chores[chore_index].chore_user_pool[
-    this.group_chores[chore_index].chore_assigned_user_index
-  ];
+    (this.group_chores[chore_index].chore_assigned_user_index + 1) %
+    this.group_chores[chore_index].chore_user_pool.length;
+  this.group_chores[chore_index].chore_assigned_user = this.group_chores[
+    chore_index
+  ].chore_user_pool[this.group_chores[chore_index].chore_assigned_user_index];
   if (save) this.save(cb);
 };
 

6b07b5f0cbced5e190e53f7b864595ec570c7ae6
Taylor Bean <JacobBean18@gmail.com>
2021-04-21 19:55:45 -0400

    useless imports

--- frontend/src/hooks/useChoreCreation.js
+++ frontend/src/hooks/useChoreCreation.js
@@ -1,5 +1,4 @@
 import axios from "axios";
-import { useState } from "react";
 import { useMutation, queryClient } from "react-query";
 import { useAPIErrorChecking } from "./useAPIErrorChecking";
 

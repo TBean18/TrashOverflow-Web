12d662e2023b1933aa926a1ce3808e30f95ffa3a
Taylor Bean <JacobBean18@gmail.com>
2021-04-22 19:49:36 -0400

    formatting

--- models/schedule.js
+++ models/schedule.js
@@ -1,59 +1,62 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
-const GroupMember = require('./groupMember');
+const GroupMember = require("./groupMember");
 
-function validateReccurenceType(t){
-  const legalVals = new Set(['DAILY', 'WEEKLY', 'MONTHLY']);
+function validateReccurenceType(t) {
+  const legalVals = new Set(["DAILY", "WEEKLY", "MONTHLY"]);
   return legalVals.has(t.reccurence_name);
 }
-const recurrenceValidator = [validateReccurenceType, '{PATH} does not support value of {VALUE}']
+const recurrenceValidator = [
+  validateReccurenceType,
+  "{PATH} does not support value of {VALUE}",
+];
 
 const reccurenceSchema = new Schema({
   reccurence_name: String,
-  reccurence_days: Number
-})
+  reccurence_days: Number,
+});
 
 const ScheduleSchema = new Schema({
   schedule_due_date: Date,
   schedule_user_rotation_type: {
     type: String,
-    default: 'iterate'
+    // default: 'iterate'
   },
   schedule_recurrence_type: {
     type: reccurenceSchema,
-    validator: recurrenceValidator
+    validator: recurrenceValidator,
+    default: null,
   },
-  date_created:{
+  date_created: {
     type: Date,
-    default: Date.now
-  }
-
-})
+    default: Date.now,
+  },
+});
 
 //Schema Functions ----------------------------------
 //Function used to set the new due date based off of the reccurance_type property
-ScheduleSchema.methods.setNewDueDate = function(cb){
-  console.log("WARNING: schedule.setNewDueDate use is deprecated, please use group.getNewDueDate instead");
+ScheduleSchema.methods.setNewDueDate = function (cb) {
+  console.log(
+    "WARNING: schedule.setNewDueDate use is deprecated, please use group.getNewDueDate instead"
+  );
   const currentDate = new Date(this.schedule_due_date);
-  switch(this.schedule_recurrence_type.reccurence_name){
-    case 'DAILY':
+  switch (this.schedule_recurrence_type.reccurence_name) {
+    case "DAILY":
       currentDate.setDate(currentDate.getDate() + 1);
       this.schedule_due_date = currentDate;
       break;
-    case 'WEEKLY':
+    case "WEEKLY":
       currentDate.setDate(currentDate.getDate() + 7);
       this.schedule_due_date = currentDate;
       break;
-    case 'MONTHLY':
+    case "MONTHLY":
       currentDate.setDate(currentDate.getDate() + 31);
       this.schedule_due_date = currentDate;
       break;
   }
   this.save(cb);
-  return
-}
+  return;
+};
 
-
-
-const model = mongoose.model('schedule', ScheduleSchema)
-module.exports = {model, ScheduleSchema}
+const model = mongoose.model("schedule", ScheduleSchema);
+module.exports = { model, ScheduleSchema };

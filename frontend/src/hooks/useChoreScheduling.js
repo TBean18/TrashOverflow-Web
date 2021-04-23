ae7be88f1f15feaa1576a6b9f60fe4d0612fca97
Taylor Bean <JacobBean18@gmail.com>
2021-04-23 19:01:03 -0400

    renamed

---
+++ frontend/src/hooks/useChoreScheduling.js
@@ -0,0 +1,44 @@
+import axios from "axios";
+import { useMutation, useQueryClient } from "react-query";
+import { useAPIErrorChecking } from "./useAPIErrorChecking";
+
+export const useChoreScheduling = (group_ID) => {
+  const errCheck = useAPIErrorChecking();
+  const queryClient = useQueryClient();
+
+  // Required Parameters
+  //      user_ID:                        String - ID of the group admin editing the schedule
+  //      group_ID:                       String - ID of the group
+  //      chore_ID:                       String - ID of the chore
+  // Optional Parameters
+  //      schedule_due_date:              Date - new date chore needs to be finished by
+  //      schedule_recurrence_type:       recurrenceSchema - new recurrence for chore
+
+  const postNewSchedule = (newSchedule) => {
+    return axios
+      .post("api/schedules/add", newSchedule)
+      .then((res) => res.data)
+      .catch((err) => errCheck(err));
+  };
+
+  const mutation = useMutation(postNewSchedule, {
+    onSuccess: (newChore) => {
+      queryClient.setQueryData([group_ID, "chores"], (current) => {
+        current.chores = newChore.chores;
+        return current;
+      });
+    },
+  });
+
+  // Required Parameters
+  //      user_ID:                        String - ID of the group admin editing the schedule
+  //      group_ID:                       String - ID of the group
+  //      chore_ID:                       String - ID of the chore
+  // Optional Parameters
+  //      schedule_due_date:              Date - new date chore needs to be finished by
+  //      schedule_recurrence_type:       recurrenceSchema - new recurrence for chore
+
+  return (newChore) => {
+    mutation.mutate(newChore);
+  };
+};

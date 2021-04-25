77cd5720a4b9292f6c0b5a70307bd5afa2bb1c61
Jo Johnson <jojohnson.jsj@gmail.com>
2021-04-25 01:10:08 -0400

    Fixed sidebar links and shit to be the actual things we need

--- frontend/src/js/MobileSidebar/MobileSidebar.js
+++ frontend/src/js/MobileSidebar/MobileSidebar.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useContext } from "react";
 import {
   SidebarContainer,
   Icon,
@@ -9,11 +9,39 @@ import {
   SideButtonWrap,
   SidebarRoute,
 } from "./MobileSidebarElements";
-
+import { GlobalContext } from "../../context/GlobalState";
 import useLogout from "../../hooks/useLogout";
+import useGroupLeave from "../../hooks/useGroupLeave";
 
 const Sidebar = ({ isOpen, toggle }) => {
   const logout = useLogout();
+  const leaveGroup = useGroupLeave(GlobalContext);
+  const [linkCopied, setLinkCopied] = useState(false);
+  const { user, currentGroup } = useContext(GlobalContext);
+
+  function copyLink() {
+    setLinkCopied(true);
+    // let link = "http://trashoverflow.tech/join/";
+    let link = "localhost:3000/join/";
+    if (currentGroup) {
+      link += currentGroup._id;
+    }
+    navigator.clipboard.writeText(link);
+    setTimeout(
+      function () {
+        setLinkCopied(false);
+      }.bind(this),
+      2000
+    );
+  }
+
+  //Function used to leave the currently slected group
+  function doLeaveGroup() {
+    let leaving = window.confirm("You are Leaving a Group");
+    if (!leaving) return;
+    leaveGroup(currentGroup._id);
+  }
+
   return (
     <SidebarContainer isOpen={isOpen} onClick={toggle}>
       <Icon onClick={toggle}>
@@ -21,18 +49,17 @@ const Sidebar = ({ isOpen, toggle }) => {
       </Icon>
       <SidebarWrapper>
         <SidebarMenu>
-          <SidebarLink to="about" onClick={toggle}>
-            About
-          </SidebarLink>
-          <SidebarLink to="discover" onClick={toggle}>
-            Our App
-          </SidebarLink>
-          <SidebarLink to="services" onClick={toggle}>
-            Features
-          </SidebarLink>
-          <SidebarLink to="mobile" onClick={toggle}>
-            <SidebarRoute to="/mobilemembers">View Members</SidebarRoute>
-          </SidebarLink>
+          <SideButtonWrap>
+            <div className="mobileSidebarText" onClick={copyLink}>
+              Get Invite Link
+            </div>
+          </SideButtonWrap>
+          <SidebarRoute className="mobileSidebarText" to="/mobilemembers">View Members</SidebarRoute>          
+          <SideButtonWrap>
+            <div className="mobileSidebarText" onClick={doLeaveGroup}>
+              Leave Group
+            </div>
+          </SideButtonWrap>
         </SidebarMenu>
         <SideButtonWrap>
           <div className="logOut" onClick={logout}>
--- frontend/src/js/MobileSidebar/MobileSidebarElements.js
+++ frontend/src/js/MobileSidebar/MobileSidebarElements.js
@@ -10,7 +10,8 @@ export const SidebarContainer = styled.aside`
   width: 100%;
   height: 100%;
   background: black;
-  display: grid;
+  display: flex;
+  justify-content: center;
   align-items: center;
   top: 0;
   left: 0;
@@ -42,8 +43,12 @@ export const SidebarWrapper = styled.div`
 export const SidebarMenu = styled.ul`
   box-sizing: border-box;
   margin: 0;
+  margin-top: 50px;
   padding: 0;
-  display: grid;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
   // controls how far from the left the items are
   grid-template-columns: 1fr;
   // spacing between titles in navbar menu. 6 rows by 80 pixels, each.
@@ -79,7 +84,7 @@ export const SidebarLink = styled(LinkS)`
 export const SideButtonWrap = styled.div`
   display: flex;
   justify-content: center;
-
+  margin-top: 50px;
   .logOut {
     border-radius: 50px;
     // trim color
@@ -94,16 +99,37 @@ export const SideButtonWrap = styled.div`
     cursor: pointer;
     transition: all 0.2s ease-in-out;
     text-decoration: none;
-
+    margin-top: 50px;
     &:hover {
       transition: all 0.2s ease-in-out;
       background: #fff;
       color: #010606;
     }
   }
+
+  .mobileSidebarText {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    font-size: 1.5rem;
+    text-decoration: none;
+    list-style: none;
+    transition: 0.2s ease-in-out;
+    text-decoration: none;
+    color: #fff;
+    cursor: pointer;
+
+    // hover action
+    &:hover {
+      // trim color
+      color: #feb054;
+      transition: 0.2s ease-in-out;
+    }
+  }
 `;
 
 export const SidebarRoute = styled(LinkR)`
+  margin-top: 50px;
   display: flex;
   align-items: center;
   justify-content: center;

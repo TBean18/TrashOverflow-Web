09322c53bfc7215f579c86d0f9fe0299cba759dd
Taylor Bean <JacobBean18@gmail.com>
2021-04-26 02:20:20 -0400

    new editorHook with queryData updating

--- frontend/src/hooks/useChoreEditor.js
+++ frontend/src/hooks/useChoreEditor.js
@@ -1,8 +1,10 @@
 import axios from "axios";
+import { useMutation, useQueryClient } from "react-query";
 import { useAPIErrorChecking } from "./useAPIErrorChecking";
 
-export const useChoreEditor = () => {
+export const useChoreEditor = (group_ID) => {
   const errCheck = useAPIErrorChecking();
+  const queryClient = useQueryClient();
 
   //This function calls the edit endpoint
   // Parameters:
@@ -11,9 +13,31 @@ export const useChoreEditor = () => {
   //      chore_name: Can be left empty
   //      chore_description: Mandatory but can be [] *empty array*
   //      chore_point_value
-  return (newChore) =>
-    axios
-      .post("/api/chores/edit", newChore)
-      .then((res) => res.data)
-      .catch((err) => errCheck(err));
+
+  const mutation = useMutation(
+    (newChore) =>
+      axios
+        .post("/api/chores/edit", newChore)
+        .then((res) => res.data)
+        .catch((err) => errCheck(err)),
+    {
+      onSuccess: (newChore) => {
+        // set the chore data
+        queryClient.setQueryData([group_ID, "chores"], (current) => {
+          current.chores = current.chores.map((chore) => {
+            // Find the edited chore
+            if (chore._id === newChore._id) {
+              // console.log("chore updated in useChoreEditor");
+
+              // And Swap it
+              return newChore;
+            } else return chore;
+          });
+          return current;
+        });
+      },
+    }
+  );
+
+  return (newChore) => mutation.mutate(newChore);
 };
